101300e3107ee0410d4bf85fcd2345f34d53b776
Yuri P <ypedroso8@gmail.com>
2019-02-20 18:57:32 -0300

    Updates

---
+++ precache-manifest.7ad93eee7ee36e2c4ae50fcc1251b7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/PWACalculator/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  },
+  {
+    "revision": "00fe597f75c49bd67467",
+    "url": "/PWACalculator/static/js/runtime~main.00fe597f.js"
+  },
+  {
+    "revision": "76a53c935ed6aa839c08",
+    "url": "/PWACalculator/static/js/main.76a53c93.chunk.js"
+  },
+  {
+    "revision": "3c63d87c98fec89dab74",
+    "url": "/PWACalculator/static/js/2.3c63d87c.chunk.js"
+  },
+  {
+    "revision": "76a53c935ed6aa839c08",
+    "url": "/PWACalculator/static/css/main.5ba206f1.chunk.css"
+  },
+  {
+    "revision": "c6e49b9f058844a1c6592f945d805958",
+    "url": "/PWACalculator/index.html"
+  }
+];
